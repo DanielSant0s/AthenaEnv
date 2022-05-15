82e9534299dc2ac4b4ad0307360017b965c7bfa0
DanielSant0s <danielsantos346@gmail.com>
2022-05-15 19:49:49 -0300

    Finish basic file manager

--- bin/file_manager.js
+++ bin/file_manager.js
@@ -3,6 +3,7 @@
 
 file_manager = new App();
 file_manager.data = [0, 0, 0, 1];
+file_manager.comp = 0;
 
 var path = "host:"
 var file = System.listDir(path);
@@ -18,19 +19,30 @@ file_manager.process = function() {
     }
     if(file_manager.data[3] == 1){
         file_manager.data[0] = process_list_commands(file_manager.data[0], file);
+        if(file_manager.data[0] > 8){
+            file_manager.comp = -(file_manager.data[0]-8)
+        } else {
+            file_manager.comp = 0;
+        }
     }
+    if ((Pads.check(pad, PAD_TRIANGLE) && !Pads.check(oldpad, PAD_TRIANGLE))){
+        path = path.slice(0, path.lastIndexOf("/"));
+        file = System.listDir(path);
+    };
     if ((Pads.check(pad, PAD_CROSS) && !Pads.check(oldpad, PAD_CROSS)) || keyboard.hole){
         if(file_manager.data[3] == 1){
-            if(!keyboard.hole){
-                path = path + "/" + file[file_manager.data[0]].name + "/";
+            if(file[file_manager.data[0]].dir){
+                if(!keyboard.hole){
+                    path = path + "/" + file[file_manager.data[0]].name;
+                }
+                file = System.listDir(path);
             }
-            file = System.listDir(path);
         }
         if(file_manager.data[2] == 1){
             switch (file_manager.data[1]) {
                 case 0:
                     fileop = 0;
-                    src = path + file[file_manager.data[0]].name;
+                    src = path + "/" + file[file_manager.data[0]].name;
                     srcfile = file[file_manager.data[0]].name;
                     file_manager.data[2] ^= 1;
                     file_manager.data[3] ^= 1;
@@ -38,14 +50,14 @@ file_manager.process = function() {
                     break;
                 case 1:
                     fileop = 1;
-                    src = path + file[file_manager.data[0]].name;
+                    src = path + "/" + file[file_manager.data[0]].name;
                     srcfile = file[file_manager.data[0]].name;
                     file_manager.data[2] ^= 1;
                     file_manager.data[3] ^= 1;
                     file = System.listDir(path);
                     break;
                 case 2:
-                    dst = path + srcfile;
+                    dst = path + "/" + srcfile;
                     switch (fileop){
                         case 0:
                             System.copyFile(src, dst);
@@ -61,7 +73,7 @@ file_manager.process = function() {
                 case 3:
                     console.log("Hole " + keyboard.hole +"\n");
                     if(keyboard.hole){
-                        System.rename(path+file[file_manager.data[0]].name, path+keyboard.getinput());
+                        System.rename(path + "/"+file[file_manager.data[0]].name, path + "/"+keyboard.getinput());
                         keyboard.hole = false;
                         file_manager.data[2] ^= 1;
                         file_manager.data[3] ^= 1;
@@ -72,7 +84,7 @@ file_manager.process = function() {
                     }
                     break;
                 case 4:
-                    System.removeFile(path + file[file_manager.data[0]].name);
+                    System.removeFile(path + "/" + file[file_manager.data[0]].name);
                     file_manager.data[2] ^= 1;
                     file_manager.data[3] ^= 1;
                     file = System.listDir(path);
@@ -93,15 +105,18 @@ file_manager.graphics.w = 350
 file_manager.graphics.h = 200
 
 render_filelist = function() {
-    Graphics.drawRect(file_manager.graphics.x, file_manager.graphics.y+(20*(file_manager.data[0]+1)), file_manager.graphics.w, 20, Color.new(64, 0, 128, 64));
+    Graphics.drawRect(file_manager.graphics.x, file_manager.graphics.y+(20*(file_manager.data[0]+2+file_manager.comp)), file_manager.graphics.w, 20, Color.new(64, 0, 128, 64));
+    Graphics.drawRect(file_manager.graphics.x, file_manager.graphics.y+(20), file_manager.graphics.w, 20, Color.new(64, 64, 64, 64));
+    printCentered(file_manager.graphics.x+(file_manager.graphics.w/2), file_manager.graphics.y+(3+(20.0)), 0.55, path);
     for (var i = 0; i < file.length; i++) {
-        Font.fmPrint(file_manager.graphics.x+10, file_manager.graphics.y+(3+(20.0*(i+1))), 0.55, 
-        file[i].name);
-        if(!file[i].dir){
-            Font.fmPrint(file_manager.graphics.x+200, file_manager.graphics.y+(3+(20.0*(i+1))), 0.55, 
-            String(file[i].size));
+        if(i+file_manager.comp < 9 && i+file_manager.comp >= 0){
+            Font.fmPrint(file_manager.graphics.x+10, file_manager.graphics.y+(3+(20.0*(i+2+file_manager.comp))), 0.55, 
+            file[i].name);
+            if(!file[i].dir){
+                Font.fmPrint(file_manager.graphics.x+200, file_manager.graphics.y+(3+(20.0*(i+2+file_manager.comp))), 0.55, 
+                String(file[i].size));
+            }
         }
-        
     };
     if(file_manager.data[2] == 1) {
         Graphics.drawRect(file_manager.graphics.x+file_manager.graphics.w, file_manager.graphics.y+20, 75, 20*5, Color.new(0, 0, 0, 100));
