274432b9f654b04ec16d1d9a8a18551874042e59
Matías Israelson <57065102+israpps@users.noreply.github.com>
2023-04-09 23:07:43 -0300

    add HDD stuff

---
+++ src/include/strUtils.h
@@ -0,0 +1,24 @@
+#ifndef STRUTILS_H
+#define STRUTILS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/**
+ * @brief  method returns true if it can extract needed info from path, otherwise false.
+ * In case of true, it updates mountString, mountPoint and newCWD parameters with corresponding data if they are not NULL
+ * It splits path by ":", and requires a minimum of 3 elements
+ * @example if path = "hdd0:__common:pfs:/retroarch/" then: mountString = "pfs:", mountPoint = "hdd0:__common", newCWD = "pfs:/retroarch/"
+ * @param path input parameter with full hdd path (`hdd0:__common:pfs:/retroarch/`)
+ * @param mountString pointer to char* wich will contain pfs mountpoint (`pfs:`)
+ * @param mountPoint returns the path of mounted partition (`hdd0:__common`)
+ * @param newCWD returns the path to the file as pfs mount point string (`pfs:/retroarch/`)
+ * @return true on success
+*/
+int getMountInfo(char *path, char *mountString, char *mountPoint, char *newCWD);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
--- src/main.c
+++ src/main.c
@@ -7,9 +7,14 @@
 #include <stdbool.h>
 
 #include "include/def_mods.h"
+#include "include/strUtils.h"
 
 #include "ath_env.h"
 #include "include/graphics.h"
+
+#define NEWLIB_PORT_AWARE
+#include <fileXio_rpc.h>
+#include <fileio.h>
 
 char boot_path[255];
 bool dark_mode;
@@ -82,10 +87,26 @@ bool waitUntilDeviceIsReady(char *path) {
 }
 
 int main(int argc, char **argv) {
+    char MountPoint[32+6+1]; // max partition name + 'hdd0:/' = '\0' 
+    char newCWD[255];
     prepare_IOP();
     init_drivers();
     
     getcwd(boot_path, sizeof(boot_path));
+    if ((!strncmp(boot_path, "hdd0:", 5)) && (strstr(boot_path, ":pfs:") != NULL) && HDD_USABLE) // we booted from HDD and our modules are loaded and running...
+    {
+        if (getMountInfo(boot_path, NULL, MountPoint, newCWD)) // ...if we can parse the boot path...
+        {
+            if (mnt(MountPoint, 0, FIO_MT_RDWR)==0) // ...mount the partition...
+            {
+                strcpy(boot_path, newCWD); // ...replace boot path with mounted pfs path.
+#ifdef RESERVE_PFS0
+                bootpath_is_on_HDD = 1;
+#endif
+            }
+
+        }
+    }
     waitUntilDeviceIsReady(boot_path);
     
     init_taskman();
@@ -147,4 +168,32 @@ int main(int argc, char **argv) {
 	// End program.
 	return 0;
 
+}
+
+
+int mnt(const char* path, int index, int openmod)
+{
+    char PFS[5+1] = "pfs0:";
+    if (index > 0)
+        PFS[3] = '0' + index;
+
+    dbgprintf("Mounting '%s' into pfs%d:\n", path, index);
+    if (fileXioMount(PFS, path, openmod) < 0) // mount
+    {
+        dbgprintf("Mount failed. unmounting & trying again...\n");
+        if (fileXioUmount(PFS) < 0) //try to unmount then mount again in case it got mounted by something else
+        {
+            dbgprintf("Unmount failed!!!\n");
+        }
+        if (fileXioMount(PFS, path, openmod) < 0)
+        {
+            dbgprintf("mount failed again!\n");
+            return -1;
+        } else {
+            dbgprintf("Second mount succed!\n");
+        }
+    } else {
+        dbgprintf("mount successfull on first attemp\n");
+    }
+    return 0;
 }
