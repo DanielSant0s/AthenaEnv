a5bb9a07d694a3a94c4ecdf141f22279e91be5f5
Viniciuscgobbi <vinicius.cgobbi2004@gmail.com>
2023-04-19 12:15:47 -0300

    feat: cd: Error if directory does not exist and args[0] is empty back to boot_path

--- bin/usr/bin/cd.js
+++ bin/usr/bin/cd.js
@@ -1,1 +1,11 @@
-System.currentDir(args[0] + "/");
+
+if (!args[0]){
+    System.currentDir(System.boot_path)
+} else{
+    if (System.doesFileExist(args[0])){
+        System.currentDir(args[0] + "/")
+    }else{
+        Console.print("cd: File or Directory Not Found: " + args[0] + "\n")
+    }
+    
+}
