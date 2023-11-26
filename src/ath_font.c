7647df568b7e05f8c3cc141240b82c472c2c4f86
Daniel Santos <47725160+DanielSant0s@users.noreply.github.com>
2023-11-26 11:44:46 -0300

    Add font render class

--- src/ath_font.c
+++ src/ath_font.c
@@ -9,9 +9,17 @@
 #include "include/fntsys.h"
 #include "ath_env.h"
 
-const uint32_t osdsys_font = 0;
-const uint32_t image_font = 1;
-const uint32_t truetype_font = 2;
+enum {
+    osdsys_font,
+    image_font,
+    truetype_font
+} FontTypes;
+
+enum {
+    ALIGN_LEFT,
+    ALIGN_CENTER,
+    ALIGN_RIGHT
+} FontAlign;
 
 typedef struct {
     uint32_t type;
@@ -21,7 +29,15 @@ typedef struct {
     float scale;
 } JSFontData;
 
+typedef struct {
+    JSFontData* font_data;
+    const char* text;
+    uint8_t align;
+    Coords size;
+} JSFontRenderData;
+
 static JSClassID js_font_class_id;
+static JSClassID js_fontrender_class_id;
 
 static void athena_font_dtor(JSRuntime *rt, JSValue val){
     JSFontData *font = JS_GetOpaque(val, js_font_class_id);
@@ -176,18 +192,93 @@ static JSValue athena_font_set_color(JSContext *ctx, JSValueConst this_val, JSVa
     return JS_UNDEFINED;
 }
 
+static JSValue athena_render_font(JSContext *ctx, JSValueConst this_val, int argc, JSValueConst *argv){
+    JSFontRenderData* render_data;
+    JSValue obj = JS_UNDEFINED;
+    JSValue proto;
+
+    obj = JS_NewObjectClass(ctx, js_fontrender_class_id);
+	if (!JS_IsException(obj)) {
+	    render_data = js_mallocz(ctx, sizeof(*render_data));
+        if (!render_data)
+            return JS_EXCEPTION;
+
+        render_data->font_data = JS_GetOpaque2(ctx, this_val, js_font_class_id);
+        render_data->text = JS_ToCString(ctx, argv[0]);
+
+        if (argc == 2) {
+            JS_ToUint32(ctx, &render_data->align, argv[1]);
+        }
+
+        if (render_data->font_data->type == 2) {
+            render_data->size = fntGetTextSize(render_data->font_data->id, render_data->text);
+        } else if (render_data->font_data->type == 0) {
+            render_data->size.width = strlen(render_data->text) * render_data->font_data->scale * 0.68f * 26.0f;
+            render_data->size.height = 26.0f * render_data->font_data->scale;
+        }
+
+        JS_SetOpaque(obj, render_data);
+        return obj;
+    }
+
+    js_free(ctx, render_data);
+    return JS_EXCEPTION;
+}
+
+static JSValue athena_fontrender_print(JSContext *ctx, JSValue this_val, int argc, JSValueConst *argv) {
+    float x, y;
+
+    JSFontRenderData *render_data = JS_GetOpaque2(ctx, this_val, js_fontrender_class_id);
+
+    JS_ToFloat32(ctx, &x, argv[0]);
+	JS_ToFloat32(ctx, &y, argv[1]);
+
+    if (render_data->font_data->type == 1){
+	    printFontText(render_data->font_data->data, render_data->text, x, y, render_data->font_data->scale, render_data->font_data->color);
+    } else if (render_data->font_data->type == 0){
+        printFontMText(render_data->text, x, y, render_data->font_data->scale, render_data->font_data->color);
+    } else {
+        fntRenderString(render_data->font_data->id, x, y, 0, 0, 0, render_data->text, render_data->font_data->color);
+    }
+
+	return JS_UNDEFINED;
+}
+
+static JSClassDef js_fontrender_class = {
+    "FontRender",
+    //.finalizer = js_std_file_finalizer,
+}; 
+
+static const JSCFunctionListEntry js_fontrender_proto_funcs[] = {
+    JS_CFUNC_DEF("print", 2, athena_fontrender_print),
+};
+
 static JSClassDef js_font_class = {
     "Font",
     .finalizer = athena_font_dtor,
 }; 
 
 static const JSCFunctionListEntry js_font_proto_funcs[] = {
-    //JS_CGETSET_MAGIC_DEF("x", js_point_get_xy, js_point_set_xy, 0),
     JS_CGETSET_DEF("scale", athena_font_get_scale, athena_font_set_scale),
     JS_CGETSET_DEF("color", athena_font_get_color, athena_font_set_color),
     JS_CFUNC_DEF("print", 3, athena_font_print),
+    JS_CFUNC_DEF("render", 3, athena_render_font),
     JS_CFUNC_DEF("getTextSize", 1, athena_font_gettextsize),
 };
+
+static void js_renderfont_init(JSContext *ctx)
+{
+    JSValue proto;
+
+    /* the class ID is created once */
+    JS_NewClassID(&js_fontrender_class_id);
+    /* the class is created once per runtime */
+    JS_NewClass(JS_GetRuntime(ctx), js_fontrender_class_id, &js_fontrender_class);
+    proto = JS_NewObject(ctx);
+    JS_SetPropertyFunctionList(ctx, proto, js_fontrender_proto_funcs,
+                               countof(js_fontrender_proto_funcs));
+    JS_SetClassProto(ctx, js_fontrender_class_id, proto);
+}
 
 static int font_init(JSContext *ctx, JSModuleDef *m) {
     JSValue font_proto, font_class;
@@ -205,6 +296,8 @@ static int font_init(JSContext *ctx, JSModuleDef *m) {
     JS_SetClassProto(ctx, js_font_class_id, font_proto);
                       
     JS_SetModuleExport(ctx, m, "Font", font_class);
+
+    js_renderfont_init(ctx);
 
     return 0;
 }
