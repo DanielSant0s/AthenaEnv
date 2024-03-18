cffb77919c27e2df822378a3ffb7405f3921068a
KreitinnSoftware <80591934+KreitinnSoftware@users.noreply.github.com>
2024-03-18 13:20:19 -0300

    Introduze 'athena_get_position', 'athena_set_position', rename 'athena_duration' for 'athena_get_duration' (#49)
    * Introduze 'athena_get_position', 'athena_set_position', rename 'athena_duration' for 'athena_get_duration'
    
    * Update Music Player, Now uses the new functions
    
    * Use resume instead of play, and only only go forward or backwards if sound is playing

--- bin/sound.js
+++ bin/sound.js
@@ -4,9 +4,9 @@ const segoe_ui = new Font("fonts/Segoe UI.ttf");
 
 const purple = Color.new(64, 0, 128);
 
-Sound.setVolume(100);
+const gray = Color.new(28, 28, 28);
 
-let timer = Timer.new();
+Sound.setVolume(100);
 
 const sound_table = System.listDir("/sounds").map(file => file.name).filter(str => str.endsWith(".ogg") || str.endsWith(".wav") );
 
@@ -21,12 +21,16 @@ sound_table.forEach(name => {
 
 let cur_sound = 0;
 
-var track = Sound.load("sounds/" + sounds[0].file);
-var duration = Sound.duration(track);
+let track = Sound.load("sounds/" + sounds[0].file);
+
+let duration = Sound.getDuration(track);
+
+let position = Sound.getPosition(track);
+
 Sound.play(track);
-Timer.reset(timer);
 
 let repeat = false;
+
 let pad = Pads.get();
 
 const icons = {play:new Image("amp/play.png"), pause:new Image("amp/pause.png"), next:new Image("amp/next.png"), back:new Image("amp/back.png")};
@@ -48,78 +52,95 @@ icons.back.height /= 3;
 
 let playing = true;
 
-var cur_duration = 0.0f;
+let cur_duration = 0.0f;
 
 let text_size = null;
 
 console.log(JSON.stringify(Tasks.get()));
 
+function msToSecondMin(ms) {
+    let minutes = Math.floor(ms / 60000)
+    let seconds = Math.round(ms / 1000)
+
+    seconds -= minutes * 60
+
+    if (seconds < 10) {
+        return minutes + ":0" + seconds
+    } else {
+        return minutes + ":" + seconds
+    }
+}
+
 while(true){
-    Screen.clear(Color.new(32, 32, 32));
+
+    Screen.clear(gray);
+
     pad.update();
 
     if(pad.justPressed(Pads.START)) {
         if(Sound.isPlaying()) {
             Sound.pause(track);
-            Timer.pause(timer);
             playing = false;
         } else {
             Sound.resume(track);
-            Timer.resume(timer);
             playing = true;
         }
     }
 
-    if(pad.justPressed(Pads.LEFT)) {
+    if(pad.justPressed(Pads.L1)) {
         cur_duration = 0;
         Sound.pause(track);
         sounds.unshift(sounds.pop());
         let new_track = Sound.load("sounds/" + sounds[0].file);
-        duration = Sound.duration(new_track);
-        Timer.reset(timer);
+        duration = Sound.getDuration(new_track);
         Sound.play(new_track);
         let old_track = track;
         track = new_track;
         Sound.free(old_track);
     }
 
-    if(pad.justPressed(Pads.RIGHT)) {
+    if(pad.justPressed(Pads.R1)) {
         cur_duration = 0;
         Sound.pause(track);
         sounds.push(sounds.shift());
         let new_track = Sound.load("sounds/" + sounds[0].file);
-        duration = Sound.duration(new_track);
-        Timer.reset(timer);
+        duration = Sound.getDuration(new_track);
         Sound.play(new_track);
         let old_track = track;
         track = new_track;
         Sound.free(old_track);
     }
 
-    if(pad.justPressed(Pads.TRIANGLE)) {
-        break;
+    position = Sound.getPosition(track);
+
+    if(pad.justPressed(Pads.RIGHT) && Sound.isPlaying()) {
+        Sound.setPosition(track, Sound.getPosition(track) + 5000);
+    } else if(pad.justPressed(Pads.LEFT) && Sound.isPlaying()) {
+        Sound.setPosition(track, Sound.getPosition(track) - 5000);
     }
 
-    if(playing) {
+    if(pad.justPressed(Pads.TRIANGLE)) {
+        Sound.restart(track);
+    }
+    
+    if(Sound.isPlaying()) {
         icons.pause.draw(320 - icons.play.width/2, 224 - icons.play.height/2 + 100);
     } else {
         icons.play.draw(320 - icons.play.width/2, 224 - icons.play.height/2 + 100);
     }
 
-    if (cur_duration < 1) {
-        cur_duration = (Math.floor(Timer.getTime(timer))/duration);
-    }
+    cur_duration = position/duration;
+    
+    segoe_ui.print(320 - sounds[0].size.width/2, 180 - sounds[0].size.height/2, sounds[0].name);
 
-    segoe_ui.print(320 - sounds[0].size.width/2, 224 - sounds[0].size.height/2, sounds[0].name);
+    segoe_ui.print(260, 220, msToSecondMin(Sound.getPosition(track)) + "/" + msToSecondMin(duration));
 
     Draw.rect(120, 260, 400, 8, purple);
-    Draw.rect(120, 260, 400*cur_duration, 8, Color.new(127, 0, 255));
+    Draw.rect(120, 260, 400 * cur_duration, 8, Color.new(127, 0, 255));
+    Draw.circle(120 + 400 * cur_duration, 264, 8, Color.new(127, 0, 255))
 
     icons.next.draw(320 - icons.next.width/2 + 150, 224 - icons.next.height/2 + 100);
     icons.back.draw(320 - icons.back.width/2 - 150, 224 - icons.back.height/2 + 100);
-    
+
     Screen.flip();
 }
-
-Timer.destroy(timer);
-Sound.deinit();
