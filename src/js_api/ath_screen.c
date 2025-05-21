269ccf9853d96589cca75ee256fca0ebc03ecd6b
Daniel Santos <47725160+DanielSant0s@users.noreply.github.com>
2025-05-21 00:15:27 -0300

    cleanup: remove incomplete useless functions

--- src/js_api/ath_screen.c
+++ src/js_api/ath_screen.c
@@ -145,59 +145,6 @@ static JSValue athena_setvmode(JSContext *ctx, JSValue this_val, int argc, JSVal
 	return JS_UNDEFINED;
 }
 
-static char* str_buf = NULL;
-static size_t str_len = 0;
-static size_t buf_len = 0;
-
-#define SCRLOG_LENGTH 72
-
-static JSValue athena_scrlog(JSContext *ctx, JSValue this_val, int argc, JSValueConst *argv){
-	unsigned int old_len, len = 0;
-	const char* in_str = JS_ToCStringLen(ctx, &len, argv[0]);
-	
-	if(!str_buf) {
-		str_buf = malloc(512);
-		buf_len = 512;
-		memset(str_buf, 0, buf_len);
-		fntLoadDefault(NULL);
-	}
-
-	old_len = str_len;
-	str_len += len + 1;
-
-	while (buf_len < str_len) {
-		buf_len += 512;
-		str_buf = realloc(str_buf, buf_len);
-	}
-
-	if (old_len > 0) {
-		str_buf[old_len-1] = '\n';
-	}
-
-	strcat(str_buf, in_str);
-
-	clearScreen(GS_SETREG_RGBAQ(0x00, 0x00, 0x00, 0x80, 0x00));
-
-	fntSetCharSize(0, FNTSYS_CHAR_SIZE*64, FNTSYS_CHAR_SIZE*64);
-
-	if (str_len > 0) {
-		fntRenderString(0, 0, 0, 0, 640, 448, str_buf, 0.8f, 0x80808080);
-	}
-
-	flipScreen();
-
-	return JS_UNDEFINED;
-}
-
-static JSValue athena_cls(JSContext *ctx, JSValue this_val, int argc, JSValueConst *argv){
-	if (str_len > 0) {
-		memset(str_buf, 0, str_len);
-		str_len = 0;
-	}
-
-	return JS_UNDEFINED;
-}
-
 static const JSCFunctionListEntry module_funcs[] = {
     JS_CFUNC_DEF("flip", 0, athena_flip),
     JS_CFUNC_DEF("clear", 1, athena_clear),
@@ -209,8 +156,6 @@ static const JSCFunctionListEntry module_funcs[] = {
 	JS_CFUNC_DEF("setFrameCounter", 1, athena_fcount),
 	JS_CFUNC_DEF("getMode", 0, athena_getvmode),
     JS_CFUNC_DEF("setMode", 1, athena_setvmode),
-	JS_CFUNC_DEF("log", 1, athena_scrlog),
-	JS_CFUNC_DEF("cls", 0, athena_cls),
 
 	JS_CFUNC_DEF("clearColor", 1, athena_set_clear_color),
 	JS_CFUNC_DEF("display", 1, athena_displayfunc),
